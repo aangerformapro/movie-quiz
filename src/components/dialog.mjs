9446e609f1ebde09155377a038296c1babb774ea
Aymeric Anger <daedelus.git@gmail.com>
2023-06-08 21:25:58 +0200

    working on dialogs

---
+++ src/components/dialog.mjs
@@ -0,0 +1,33 @@
+/**
+ * @link https://m2.material.io/components/dialogs/web#using-dialogs
+ * @link https://getmdl.io/components/index.html#dialog-section
+ */
+
+
+import dialogPolyfill from 'dialog-polyfill';
+
+
+
+// import { MDCDialog } from '@material/dialog';
+
+// const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
+
+
+// console.debug(dialog);
+
+// dialog.listen('MDCDialog:opened', console.debug);
+
+// dialog.listen('MDCDialog:opened', function ()
+// {
+//     // Assuming contentElement references a common parent element with the rest of the page's content
+//     // contentElement.setAttribute('aria-hidden', 'true');
+// });
+// dialog.listen('MDCDialog:closing', function (e)
+// {
+//     // contentElement.removeAttribute('aria-hidden');
+
+//     console.debug(e);
+// });
+
+
+// dialog.open();
--- src/layout.mjs
+++ src/layout.mjs
@@ -1,5 +1,5 @@
 
-
+import "./components/dialog.mjs";
 const burger = document.querySelector(".burger-btn");
 
 
@@ -13,41 +13,14 @@ burger.addEventListener("click", () =>
 
 
 const swiper = new Swiper('.swiper', {
-    // loop: true,
-    // autoHeight: true,
+
     grabCursor: true,
-    slidesPerView: 2,
-    // autoplay: {
-    //     delay: 2000,
-    //     // reverseDirection: true,
-    //     disableOnInteraction: false,
-    //     pauseOnMouseEnter: true
-    // },
-    parallax: true,
+    slidesPerView: "auto",
     freeMode: {
         enabled: true,
-        // sticky: true,
+        sticky: true,
     },
-    breakpoints: {
-        480: {
-            slidesPerView: 2
-        },
-        768: {
-            slidesPerView: 3
-        },
-
-        992: {
-            slidesPerView: 4
-        },
-
-        992: {
-            slidesPerView: 5
-        },
-        1200: {
-            slidesPerView: 6
-        }
-
+    mousewheel: {
+        releaseOnEdges: false,
     },
-
-
 });
